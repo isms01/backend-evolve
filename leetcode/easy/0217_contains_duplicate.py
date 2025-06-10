79c10ca251e44222eff743ff4d1bc5a4330fe77f
isms01 <ishimasa01@gmail.com>
2025-06-10 22:46:27 +0900

    additional answer to today's leetcode problem

---
+++ leetcode/easy/0217_contains_duplicate.py
@@ -0,0 +1,9 @@
+import os
+from typing import List
+
+
+# LeetCode Problem: 217. Contains Duplicate
+# https://leetcode.com/problems/contains-duplicate/
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        return bool(len(nums) - len(set(nums)))
