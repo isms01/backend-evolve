91b32e569c2d1ec06f3be597526fc59e35b2013d
isms01 <ishimasa01@gmail.com>
2025-06-10 22:51:37 +0900

    update to more efficient algorism

--- leetcode/easy/0217_contains_duplicate.py
+++ leetcode/easy/0217_contains_duplicate.py
@@ -6,4 +6,4 @@ from typing import List
 # https://leetcode.com/problems/contains-duplicate/
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        return bool(len(nums) - len(set(nums)))
+        return len(nums) != len(set(nums))
