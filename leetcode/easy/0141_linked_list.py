939b02afe459349b1de36d46ecbeda355d92cc1f
isms01 <ishimasa01@gmail.com>
2025-06-18 22:52:06 +0900

    Answer to Leetcode#141 question

---
+++ leetcode/easy/0141_linked_list.py
@@ -0,0 +1,21 @@
+from typing import Optional
+
+
+# LeetCode Problem: 141. Linked List Cycle
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+# https://leetcode.com/problems/linked-list-cycle/
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        cur = head
+        visited = set()
+        while cur:
+            if cur in visited:
+                return True
+            visited.add(cur)
+            cur = cur.next
+        return False
