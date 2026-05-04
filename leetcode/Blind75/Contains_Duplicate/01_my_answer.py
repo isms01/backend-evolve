478c08ed3fd2ca51d1ecbf657887a918303982f2
isms01 <ishimasa01@gmail.com>
2026-05-04 17:59:09 +0900

    add my answer for problem "contains duplicate" of Blind75

---
+++ leetcode/Blind75/Contains_Duplicate/01_my_answer.py
@@ -0,0 +1,11 @@
+from typing import List
+
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        for i in range(len(nums)):
+            for j,num in enumerate(nums):
+                if i==j:
+                    continue
+                if nums[i] == num:
+                    return True
+        return False
